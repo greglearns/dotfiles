740a07a0a8771ac47d04a9fa7b1c44f51c1d206e
Greg Edwards <greg@greglearns.com>
2014-11-04 12:40:15 -0700

    Add refresh-browser.sh.

---
+++ refresh-browser.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# osascript -e 'tell application "Google Chrome" to tell the active tab of its first window to reload'
+osascript -e 'tell application "Google Chrome Canary" to tell the active tab of its first window to reload'
+
